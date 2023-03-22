adf491d856c53a444a0191d4fb6fb09cd829a724
programadoragain <ferdandi04@gmail.com>
2023-03-22 01:47:54 -0300

    Update report functions.

--- src/main/java/com/ferdev/cafe/Service/BillServiceImpl.java
+++ src/main/java/com/ferdev/cafe/Service/BillServiceImpl.java
@@ -5,7 +5,7 @@ import com.ferdev.cafe.Entities.Bill;
 import com.ferdev.cafe.Jwt.JwtFilter;
 import com.ferdev.cafe.Repositories.BillRepository;
 import com.ferdev.cafe.Util.CafeUtils;
-import com.itextpdf.text.Document;
+import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,13 +39,22 @@ public class BillServiceImpl implements BillService {
                     requestMap.put("uuid", fileName);
                     insertBill(requestMap);
                 }
+                /* Begin Set document properties */
                 String data= "Name: " + requestMap.containsKey("name") + "\n"
                             + "Contact Number: " + requestMap.containsKey("contactNumber") + "\n"
                             + "Email: " + requestMap.containsKey("email") + "\n"
                             + "Payment Method: " + requestMap.containsKey("paymentMethod") + "\n";
 
                 Document document= new Document();
-                PdfWriter.getInstance(document, new FileOutputStream(C))
+                PdfWriter.getInstance(document, new FileOutputStream(CafeConstanst.STORE_LOCATION+ "\\"+ fileName + ".pfd"));
+
+                document.open();
+                setRectangleInPdf(document);
+
+                Paragraph title= new Paragraph("Cafe Management System", getFont("Header"));
+                title.setAlignment(Element.ALIGN_CENTER);
+                document.add(title);
+
 
 
                 return CafeUtils.getResponseEntity(".", HttpStatus.OK);
@@ -55,6 +64,33 @@ public class BillServiceImpl implements BillService {
             e.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstanst.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private Font getFont(String type) {
+        switch (type) {
+
+            case "Header" : Font headerFont= FontFactory.getFont(FontFactory.HELVETICA_BOLDOBLIQUE, 18, BaseColor.BLACK);
+                            headerFont.setStyle(Font.BOLD);
+                            return headerFont;
+
+            case "Data" :   Font dataFont= FontFactory.getFont(FontFactory.TIMES_ROMAN, 11, BaseColor.BLACK);
+                            dataFont.setStyle(Font.BOLD);
+                            return dataFont;
+
+            default :       return new Font();
+        }
+    }
+
+    private void setRectangleInPdf(Document document) throws DocumentException {
+        log.info("Inside setRectangleInPdf");
+        Rectangle rectangle= new Rectangle(577,825,18,15);
+        rectangle.enableBorderSide(1);
+        rectangle.enableBorderSide(2);
+        rectangle.enableBorderSide(3);
+        rectangle.enableBorderSide(4);
+        rectangle.setBackgroundColor(BaseColor.BLACK);
+        rectangle.setBorderWidth(1);
+        document.add(rectangle);
     }
 
     private void insertBill(Map<String, Object> requestMap) {
