9d3b9e10b7e99a7b3f49095f81d032406d974d5f
programadoragain <ferdandi04@gmail.com>
2023-03-23 01:07:22 -0300

    Update BillServiceImpl

--- src/main/java/com/ferdev/cafe/Service/BillServiceImpl.java
+++ src/main/java/com/ferdev/cafe/Service/BillServiceImpl.java
@@ -6,8 +6,11 @@ import com.ferdev.cafe.Jwt.JwtFilter;
 import com.ferdev.cafe.Repositories.BillRepository;
 import com.ferdev.cafe.Util.CafeUtils;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.FileOutputStream;
 import java.util.Map;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -39,6 +43,7 @@ public class BillServiceImpl implements BillService {
                     requestMap.put("uuid", fileName);
                     insertBill(requestMap);
                 }
+
                 /* Begin Set document properties */
                 String data= "Name: " + requestMap.containsKey("name") + "\n"
                             + "Contact Number: " + requestMap.containsKey("contactNumber") + "\n"
@@ -55,15 +60,56 @@ public class BillServiceImpl implements BillService {
                 title.setAlignment(Element.ALIGN_CENTER);
                 document.add(title);
 
+                Paragraph info= new Paragraph(data + "\n \n", getFont("Data"));
+                document.add(info);
 
+                PdfPTable table= new PdfPTable(5);
+                table.setWidthPercentage(100);
+                addTableHeader(table);
 
-                return CafeUtils.getResponseEntity(".", HttpStatus.OK);
+                JSONArray jsonArray= CafeUtils.getJsonArrayFromString((String) requestMap.get("productDetails"));
+                for (int i=0; i<jsonArray.length();i++) {
+                    addRows(table, CafeUtils.getMapFromJson(jsonArray.getString(i)));
+                }
+
+                document.add(table);
+
+                Paragraph footer= new Paragraph("Total: " + requestMap.get("total") + " \n"
+                        + "Thank you for visiting, please visit again!", getFont("Data"));
+
+                document.add(footer);
+                document.close();
+
+                return new ResponseEntity<>("{\"uuid\":\"" + fileName + "\"}", HttpStatus.OK);
             }
             return CafeUtils.getResponseEntity("Required data not found.", HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstanst.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private void addRows(PdfPTable table, Map<String, Object> data) {
+        log.info("Inside of addRows ");
+        table.addCell((String) data.get("name"));
+        table.addCell((String) data.get("category"));
+        table.addCell((String) data.get("quantity"));
+        table.addCell(Double.toString((Double) data.get("price")));
+        table.addCell(Double.toString((Double) data.get("total")));
+    }
+
+    private void addTableHeader(PdfPTable table) {
+        Stream.of("Name", "Category", "Quantity", "Price", "Sub Total")
+                .forEach(columTitle -> {
+                    PdfPCell header= new PdfPCell();
+                    header.setBackgroundColor(BaseColor.LIGHT_GRAY);
+                    header.setBorderWidth(2);
+                    header.setPhrase(new Phrase(columTitle));
+                    header.setBackgroundColor(BaseColor.YELLOW);
+                    header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    header.setVerticalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(header);
+                });
     }
 
     private Font getFont(String type) {
@@ -88,7 +134,7 @@ public class BillServiceImpl implements BillService {
         rectangle.enableBorderSide(2);
         rectangle.enableBorderSide(3);
         rectangle.enableBorderSide(4);
-        rectangle.setBackgroundColor(BaseColor.BLACK);
+        rectangle.setBorderColor(BaseColor.BLACK);
         rectangle.setBorderWidth(1);
         document.add(rectangle);
     }
--- src/main/java/com/ferdev/cafe/Util/CafeUtils.java
+++ src/main/java/com/ferdev/cafe/Util/CafeUtils.java
@@ -1,8 +1,15 @@
 package com.ferdev.cafe.Util;
 
+import com.google.common.base.Strings;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CafeUtils {
     private CafeUtils(){}
@@ -15,5 +22,17 @@ public class CafeUtils {
         Date data= new Date();
         long time= data.getTime();
         return "BILL-" + time;
+    }
+
+    public static JSONArray getJsonArrayFromString(String data) throws JSONException {
+        JSONArray jsonArray= new JSONArray(data);
+        return jsonArray;
+    }
+
+    public static Map<String, Object> getMapFromJson(String data) {
+        if (!Strings.isNullOrEmpty(data))
+            return new Gson().fromJson(data, new TypeToken<Map<String,Object>>(){}.getType());
+        else
+            return new HashMap<>();
     }
 }
